77997e400e1a307f46e466196e84c4d178c3b1b9
pporter5 <pporter5@Mint-Dev>
2012-12-03 16:07:55 -0500

    Problem 2 skeleton created before leaving work

---
+++ Problem_002/Problem_002.py
@@ -0,0 +1,20 @@
+'''
+Created on Dec 3, 2012
+
+@author: pporter5
+'''
+class Problem002:
+    '''
+    By considering the terms in the Fibonacci sequence whose values 
+    do not exceed four million (4,000,000), find the sum of the 
+    even valued terms
+    '''
+
+    def __init__(self, upperBound = 4000000):
+        '''
+        Constructor
+        '''
+        
+        
+if __name__ == '__main__':
+    pass
