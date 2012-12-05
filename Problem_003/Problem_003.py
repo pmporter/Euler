1e7bc54aece887a87ddecb43ccc09c5ba56df6df
pmpor <pmpor@cscc-iMac>
2012-12-05 16:19:54 -0500

    Whatever

--- Problem_003/Problem_003.py
+++ Problem_003/Problem_003.py
@@ -3,18 +3,16 @@ Find the largest prime factor of a composite number
 '''
 def largestPrimeFactor(num):
     factor = 2
-    
     while factor <= num:
         if(num % factor == 0):
             num = num / factor
-            factor -= 1            
+            factor -= 1     
         factor += 1
     return factor
 
-
 number = 600851475143
-print largestPrimeFactor(number)
-    
+print largestPrimeFactor(67)
+
                 
             
         
